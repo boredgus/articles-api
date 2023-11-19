4e25693160c9afd3a27a884e0c1d61e5af4f810f
Daryna Kharchenko <daha.kyiv@gmail.com>
2023-11-19 11:43:56 +0200

    feat: update user table

--- sql/user.sql
+++ sql/user.sql
@@ -1,20 +1,6 @@
 create table user (
-  id int auto_increment,
+  id int auto_increment primary key,
   o_id varchar(36) unique not null,
-  username varchar(100) unique not null,
-  pswd varchar(200) not null,
-  primary key (id)
+  username varchar(50) unique not null check (username > ''),
+  pswd varchar(60) not null check (username > '')
 );
-
--- create table user_profile (
--- 	id int,
--- 	o_id varchar(36) not null unique,
--- 	nickname varchar(255) not null unique,
---     first_name varchar(255) default "",
---     last_name varchar(255) default "",
---     created_at timestamp default current_timestamp,
---     updated_at timestamp,
---     deleted_at timestamp,
---     primary key (id),
---     foreign key (id) references user (id) on delete cascade
--- );
