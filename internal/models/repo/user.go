9c9457cbd0b5ad97a32d3ce63acc89810ee9066c
Daryna Kharchenko <daha.kyiv@gmail.com>
2023-11-24 14:11:48 +0200

    refactor: move interface declaring to another package

---
+++ internal/models/repo/user.go
@@ -0,0 +1,12 @@
+package repo
+
+type User struct {
+	OId      string `sql:"o_id"`
+	Username string `sql:"username"`
+	Password string `sql:"pswd"`
+}
+
+type UserRepository interface {
+	Create(user User) error
+	Get(username string) (User, error)
+}
--- internal/models/user.go
+++ internal/models/user.go
@@ -5,35 +5,25 @@ import (
 	"fmt"
 	"user-management/internal/auth"
 	"user-management/internal/domain"
+	"user-management/internal/models/repo"
 
 	"github.com/google/uuid"
 )
-
-type User struct {
-	OId      string `sql:"o_id"`
-	Username string `sql:"username"`
-	Password string `sql:"pswd"`
-}
 
 type UserModel interface {
 	Create(user domain.User) error
 	Authorize(user domain.User) (string, string, error)
 }
 
-type UserRepository interface {
-	Create(user User) error
-	Get(username string) (User, error)
-}
-
 var InvalidAuthParameterErr = errors.New("username or password is invalid")
 var UsernameDuplicationErr = errors.New("user with such username already exists")
 
-func NewUserModel(repo UserRepository) UserModel {
+func NewUserModel(repo repo.UserRepository) UserModel {
 	return user{repo: repo, token: auth.NewToken(), pswd: auth.NewPassword()}
 }
 
 type user struct {
-	repo  UserRepository
+	repo  repo.UserRepository
 	token auth.Token
 	pswd  auth.Password
 }
@@ -47,7 +37,7 @@ func (u user) Create(user domain.User) error {
 	if err != nil {
 		return err
 	}
-	return u.repo.Create(User{OId: uuid.New().String(), Username: user.Username, Password: hashedPswd})
+	return u.repo.Create(repo.User{OId: uuid.New().String(), Username: user.Username, Password: hashedPswd})
 }
 
 func (u user) Authorize(user domain.User) (userId string, token string, err error) {
@@ -55,8 +45,7 @@ func (u user) Authorize(user domain.User) (userId string, token string, err erro
 	if err != nil {
 		return "", "", InvalidAuthParameterErr
 	}
-	arePswdsEqual := u.pswd.Compare(userFromDB.Password, user.Password)
-	if !arePswdsEqual {
+	if !u.pswd.Compare(userFromDB.Password, user.Password) {
 		return "", "", InvalidAuthParameterErr
 	}
 
