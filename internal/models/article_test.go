3e4c8fb9eedc1f6d467e01eed962c84494a2545e
Daryna Kharchenko <daha.kyiv@gmail.com>
2023-11-30 22:48:28 +0200

    test: cover article model

---
+++ internal/models/article_test.go
@@ -0,0 +1,130 @@
+package models
+
+import (
+	"errors"
+	"testing"
+	"user-management/internal/domain"
+	repoMocks "user-management/internal/mocks/repo"
+	"user-management/internal/models/repo"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestArticleService_Create(t *testing.T) {
+	type args struct {
+		userOId string
+		article *domain.Article
+	}
+	type articleMocks struct {
+		repo repo.ArticleRepository
+	}
+	type mockedRes struct {
+		createErr error
+	}
+	repoMock := repoMocks.NewArticleRepository(t)
+	setup := func(res mockedRes) func() {
+		repoCall := repoMock.EXPECT().
+			Create(mock.Anything, mock.Anything).Return(res.createErr).Once()
+		return func() {
+			repoCall.Unset()
+		}
+	}
+	someError := errors.New("some error")
+	tests := []struct {
+		name      string
+		mockedRes mockedRes
+		args      args
+		wantErr   error
+	}{
+		{
+			name:    "article data is invalid",
+			args:    args{article: &domain.Article{Theme: ""}},
+			wantErr: InvalidArticleErr,
+		},
+		{
+			name:      "failed to insert article data to db",
+			args:      args{article: &domain.Article{Theme: "error"}},
+			mockedRes: mockedRes{createErr: someError},
+			wantErr:   someError,
+		},
+		{
+			name:    "success",
+			args:    args{article: &domain.Article{Theme: "success"}},
+			wantErr: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cleanSetup := setup(tt.mockedRes)
+			defer cleanSetup()
+			err := NewArticleModel(repoMock).Create(tt.args.userOId, tt.args.article)
+			if tt.wantErr != nil {
+				assert.ErrorIs(t, err, tt.wantErr)
+				return
+			}
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func TestArticleService_GetForUser(t *testing.T) {
+	type args struct {
+		username string
+		page     int
+		limit    int
+	}
+	type mockedRes struct {
+		getErr      error
+		getArticles []domain.Article
+	}
+	repoMock := repoMocks.NewArticleRepository(t)
+	setup := func(res mockedRes) func() {
+		repoCall := repoMock.EXPECT().
+			GetForUser(mock.Anything, mock.Anything, mock.Anything).
+			Return(res.getArticles, res.getErr).Once()
+		return func() {
+			repoCall.Unset()
+		}
+	}
+	someError := errors.New("some error")
+	articles := []domain.Article{{Theme: "theme1"}, {Theme: "theme2"}, {Theme: "theme3"}}
+	tests := []struct {
+		name           string
+		args           args
+		mockedRes      mockedRes
+		wantArticle    []domain.Article
+		wantPagination PaginationData
+		wantErr        error
+	}{
+		{
+			name:           "failed to get articles",
+			mockedRes:      mockedRes{getArticles: []domain.Article{}, getErr: someError},
+			wantArticle:    nil,
+			wantPagination: PaginationData{},
+			wantErr:        someError,
+		},
+		{
+			name:           "success",
+			args:           args{limit: 10},
+			mockedRes:      mockedRes{getArticles: articles, getErr: nil},
+			wantArticle:    articles,
+			wantPagination: PaginationData{Page: 0, Limit: 10, Count: len(articles)},
+			wantErr:        nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cleanSetup := setup(tt.mockedRes)
+			defer cleanSetup()
+			articles, pagination, err := NewArticleModel(repoMock).GetForUser(tt.args.username, tt.args.page, tt.args.limit)
+			assert.Equal(t, articles, tt.wantArticle, "articles_check")
+			assert.Equal(t, pagination, tt.wantPagination, "pagination_check")
+			if tt.wantErr != nil {
+				assert.ErrorIs(t, err, tt.wantErr, "error_check")
+				return
+			}
+			assert.Nil(t, err)
+		})
+	}
+}
